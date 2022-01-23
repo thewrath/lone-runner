76974bea65e6eebbce39a212be43b1e88cc2b5ec
Thomas <epis.ajout@gmail.com>
2022-01-23 15:05:52 +0100

    Fix - wall detection

--- main.rkt
+++ main.rkt
@@ -2,7 +2,7 @@
 
 (require r-cade)
 
-(define DEBUG #t)
+(define DEBUG #f)
 
 (define M-WIDTH 256)
 (define M-HEIGHT 256)
@@ -11,7 +11,12 @@
 (define HALF-B-SIZE (/ B-SIZE 2))
 
 (define P-SPEED 1)
-(define GRAVITY 2)
+(define GRAVITY 1)
+
+(define UP 0)
+(define RIGHT 1)
+(define DOWN 2)
+(define LEFT 3)
 
 (define debug-text "debug")
 
@@ -69,6 +74,14 @@
                               #b01000010
                               #b01000010
                               #b01000010)
+                        "db" '(
+                              #b11111111
+                              #b10000001
+                              #b10010001
+                              #b10111001
+                              #b10010001
+                              #b10000001
+                              #b11111111)
                         "w" '(
                               #b01111110
                               #b10000001	
@@ -80,7 +93,7 @@
 
 ; Define all primitive of our game
 (struct point (x y) #:transparent #:mutable)
-(struct block (position type) #:transparent)
+(struct block (position coords type) #:transparent #:mutable)
 (struct player (position block) #:transparent #:mutable)
 (struct enemy (position) #:transparent)
 (struct level (indice gold) #:transparent)
@@ -112,40 +125,40 @@
     (world p e l)))
 
 (define (update/player p l)
-  (define new-player (apply-gravity (move-player p l)))
-  (define wall (get-wall new-player))
-  (if (not (null? wall))
-    (let ([dir (get-block-direction (player-block p) (player-block new-player))])
-      (set! debug-text dir)
-      (player 
-        (point-add (player-position new-player) dir) 
-        (player-block new-player)))
-    new-player))
+  (define new-player (apply-gravity (move-player p l) l))
+  new-player)
 
 (define (move-player p l)
-  (define x (point-x (player-position p)))
-  (define y (point-y (player-position p)))
-  (define dx 0)
-  (define dy 0)
-  (when ((actions/up))
-    (set! dy -1))
-  (when ((actions/right))
-    (set! dx 1))
-  (when ((actions/down))
-    (set! dy 1))
-  (when ((actions/left))
-    (set! dx -1))
-  (player (point 
-            (+ x (* P-SPEED dx))
-            (+ y (* P-SPEED dy)))(get-block l (player-position p) #:mode "upper")))
+  (let ([x (point-x (player-position p))]
+        [y (point-y (player-position p))]
+        [dx 0]
+        [dy 0])
+    (when (and ((actions/up)) (can-player-go? p l UP))
+      (set! dy -1))
+    (when (and ((actions/right)) (can-player-go? p l RIGHT))
+      (set! dx 1))
+    (when (and ((actions/down)) (can-player-go? p l DOWN))
+      (set! dy 1))
+    (when (and ((actions/left)) (can-player-go? p l LEFT))
+      (set! dx -1))
+    (player (point
+              (+ x (* P-SPEED dx))
+              (+ y (* P-SPEED dy)))(get-block l (player-position p) #:mode "upper"))))
+
+(define (can-player-go? p l dir)
+  (let* ([blocks (get-blocks-around-player p l)]
+         [block (list-ref blocks dir)]) 
+    (and 
+      (not (null? block)) 
+      (not (equal? "w" (block-type block))))))
 
 ; Apply gravity to a player/enemy
-(define (apply-gravity p)
+(define (apply-gravity p l)
   (let* ([position (player-position p)]
         [x (point-x position)]
         [y (point-y position)]
         [b (player-block p)])
-    (if (and (< y (- M-HEIGHT (* 2 B-SIZE))) (not (equal? "l" (block-type b))))
+    (if (and (can-player-go? p l DOWN) (not (equal? "l" (block-type b))))
         (player (point x (+ y GRAVITY)) b)
         p)))
 
@@ -158,7 +171,7 @@
 
 (define (draw/world w)
   (draw/level (level-indice (world-level w)))
-  (draw/player (world-player w)))
+  (draw/player (world-player w) (level-indice (world-level w))))
 
 ; Draw a specific level (l correspond to level indice in asset/levels)
 (define (draw/level i)
@@ -171,10 +184,18 @@
   (on-level-map i draw-block))
 
 ; Draw player
-(define (draw/player p)
-  (let ([x (point-x (player-position p))]
-        [y (point-y (player-position p))])
-    (draw x y (hash-ref asset/sprites "p"))))
+(define (draw/player p l)
+  (let* ([x (point-x (player-position p))]
+         [y (point-y (player-position p))]
+         [debug-blocks (cons (player-block p) (get-blocks-around-player p l))])
+    (draw x y (hash-ref asset/sprites "p"))
+    (color 3)
+    (and DEBUG (map (lambda (b)
+                      (unless (null? b)
+                        (let ([bx (point-x (block-position b))]
+                            [by (point-y (block-position b))])
+                        (draw bx by (hash-ref asset/sprites "db"))))
+                      ) debug-blocks))))
 
 ;; HELPERS
 (define (on-level-map i f)
@@ -182,9 +203,9 @@
     (for ([l (vector-ref asset/levels i)])
       (set! x 0)
       (for ([t l])
-        (f (block (point x y) t))
-        (set! x (+ x B-SIZE)))
-      (set! y (+ y B-SIZE)))))
+        (f (block (point (* x B-SIZE) (* y B-SIZE)) (point x y) t))
+        (set! x (add1 x)))
+      (set! y (add1 y)))))
 
 (define (on-level-row i f)
   (let ([x 0])
@@ -207,30 +228,48 @@
                       (set! b current-block)))))
   b)
 
-(define (get-wall p)
-  (define b (player-block p))
-  (if (and (not (null? b)) (equal? "w" (block-type b)))
-      b
-      null))
-
 ; Get direction from cb block to db block
 (define (get-block-direction cb db)
-  (let ([cb-pos (block-position cb)]
+  (let* ([cb-pos (block-position cb)]
         [db-pos (block-position db)]
-        [dir (point 0 0)])
-    (when (> (point-x cb-pos) (point-x db-pos))
-      (set-point-x! dir HALF-B-SIZE))
-    (when (< (point-x cb-pos) (point-x db-pos))
-      (set-point-x! dir (- 0 HALF-B-SIZE)))
-    (when (> (point-y cb-pos) (point-y db-pos))
-      (set-point-y! dir HALF-B-SIZE))
-    (when (< (point-y cb-pos) (point-y db-pos))
-      (set-point-y! dir (- 0 HALF-B-SIZE)))
+        [dir (point 
+               (- (point-x cb-pos) (point-x db-pos))
+               (- (point-y cb-pos) (point-y db-pos)))])
     dir))
+
+(define (get-blocks-around-player p l)
+  (if (null? (player-block p))
+      (list)
+      (let* ([pb (player-block p)]
+             [pbc (point-x (block-coords pb))]
+             [pbl (point-y (block-coords pb))])
+      (map (lambda (b) (if (block-in-map? b l) 
+                           (begin (set-block-type! b (get-block-type b l)) b) 
+                           null)) ; This little lamba fill block type
+           (list
+              (block (point (* pbc B-SIZE) (* (sub1 pbl) B-SIZE)) (point pbc (sub1 pbl)) "e") ; up
+              (block (point (* (add1 pbc) B-SIZE) (* pbl B-SIZE)) (point (add1 pbc) pbl) "e") ; right
+              (block (point (* pbc B-SIZE) (* (add1 pbl) B-SIZE)) (point pbc (add1 pbl)) "e") ; down
+              (block (point (* (sub1 pbc) B-SIZE) (* pbl B-SIZE)) (point (sub1 pbc) pbl) "e")))))) ; left
+
+; Check if block coords are in bounds of map 
+(define (block-in-map? b l)
+  (let ([bc (point-x (block-coords b))]
+        [bl (point-y (block-coords b))]
+        [level (vector-ref asset/levels l)])
+    (and
+      (and (< bl (vector-length level)) (>= bl 0))
+      (and (< bc (vector-length (vector-ref level bl))) (>= bc 0)))))
+
+(define (get-block-type b l) 
+  (let ([bc (point-x (block-coords b))]
+        [bl (point-y (block-coords b))]
+        [level (vector-ref asset/levels l)]) 
+    (vector-ref (vector-ref level bl) bc)))
 
 ;; INIT
 (define (init)
-  (define p (player (point 10 10) null))
+  (define p (player (point 50 50) null))
   (define e (list (enemy (point 10 10))))
   (define l (level 0 100))
   (world p e l))
@@ -239,9 +278,11 @@
 
 (define (game-loop)
   (cls)
+  (color 1)
   (set! my-world (update/world my-world))
   (draw/world my-world)
-  (and DEBUG (text 50 50 debug-text)))
+  (color 3)
+  (and DEBUG (text 0 50 debug-text)))
 ;(draw 50 0 (hash-ref asset/sprites "player"))
 
 (run game-loop M-WIDTH M-HEIGHT #:scale 2 #:shader #f)
